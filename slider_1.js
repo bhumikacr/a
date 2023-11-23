d270fa6c8180b8fbdcc8d32d983cc298c4ccd4d2
bhumikacr <bhumikacr9@gmail>
2023-11-23 14:33:00 +0530

    first commit

---
+++ slider_1.js
@@ -0,0 +1,27 @@
+// Init slick slider + animation
+$('.slider').slick({
+    autoplay: true,
+    speed: 800,
+    lazyLoad: 'progressive',
+    arrows: true,
+    dots: false,
+      prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+      nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+  }).slickAnimation();
+  
+  
+  
+  $('.slick-nav').on('click touch', function(e) {
+  
+      e.preventDefault();
+  
+      var arrow = $(this);
+  
+      if(!arrow.hasClass('animate')) {
+          arrow.addClass('animate');
+          setTimeout(() => {
+              arrow.removeClass('animate');
+          }, 1600);
+      }
+  
+  });
